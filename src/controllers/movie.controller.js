5d2ebce7bd586af99d23f503ff5981c93f050f8a
anniepanny <anniepanthi@gmail.com>
2022-06-21 15:22:26 +0545

    Add business logic

--- src/controllers/movie.controller.js
+++ src/controllers/movie.controller.js
@@ -2,6 +2,6 @@ export const getAllMovies = (req, res) => {}
 export const getMovieById = (req, res) => {}
 export const getMovieByGenre = (req, res) => {}
 export const getMovieByActor = (req, res) => {}
-export const updateMovie = (req, res) => {}
-export const deleteMovie = (req, res) => {}
+export const updateMovieById = (req, res) => {}
+export const deleteMovieById = (req, res) => {}
 export const createMovie = (req, res) => {}
